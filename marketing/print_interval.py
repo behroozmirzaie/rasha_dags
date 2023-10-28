f932bb3ed8e5f80f00a2051e06eaa84453179a95
behrooz <mirzaie2@gmail.com>
2023-10-28 17:32:39 +0200

    Add dummy DAG

---
+++ marketing/print_interval.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import airflow
+
+
+def print_hello():
+    print("Hello, world!")
+
+
+default_args = {
+    'start_date': airflow.utils.dates.days_ago(2),
+    'schedule_interval': '@minute',
+}
+
+with DAG('print_hello_dag', default_args=default_args, author='Behrooz') as dag:
+    print_hello_task = PythonOperator(
+        task_id='print_hello',
+        python_callable=print_hello,
+    )
+
+    print_hello_task
