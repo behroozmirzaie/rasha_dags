2ee48650e2a913b44c9ed9d1464fa1f0fe7c9f7b
behrooz <mirzaie2@gmail.com>
2023-10-28 21:02:34 +0200

    Add postgres operator

---
+++ training/03.py
@@ -0,0 +1,39 @@
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+def print_world():
+    print('this should be run every minute')
+
+
+default_args = {
+    'owner': 'Behrooz',
+    'start_date': dt.datetime.now(),
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=1),
+    'schedule_interval': dt.timedelta(minutes=1),
+}
+
+with DAG(
+        dag_id="postgres_operator_dag",
+        start_date=dt.datetime.now(),
+        schedule_interval="@once",
+        catchup=False,
+) as dag:
+    create_pet_table = PostgresOperator(
+        database="beh1",
+        host="",
+        task_id="create_pet_table",
+        sql="""
+            CREATE TABLE IF NOT EXISTS pet (
+            pet_id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            pet_type VARCHAR NOT NULL,
+            birth_date DATE NOT NULL,
+            OWNER VARCHAR NOT NULL);
+          """,
+    )
