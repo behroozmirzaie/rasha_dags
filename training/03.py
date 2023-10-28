b5bc2b50e244f35bf2ba5d0ec20d889917daab28
behrooz <mirzaie2@gmail.com>
2023-10-28 21:05:33 +0200

    Add exception

--- training/03.py
+++ training/03.py
@@ -24,14 +24,17 @@ with DAG(
         schedule_interval="@once",
         catchup=False,
 ) as dag:
-    create_pet_table = PostgresOperator(
-        task_id="create_pet_table",
-        sql="""
-            CREATE TABLE IF NOT EXISTS pet (
-            pet_id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            pet_type VARCHAR NOT NULL,
-            birth_date DATE NOT NULL,
-            OWNER VARCHAR NOT NULL);
-          """,
-    )
+    try:
+        create_pet_table = PostgresOperator(
+            task_id="create_pet_table",
+            sql="""
+                CREATE TABLE IF NOT EXISTS pet (
+                pet_id SERIAL PRIMARY KEY,
+                name VARCHAR NOT NULL,
+                pet_type VARCHAR NOT NULL,
+                birth_date DATE NOT NULL,
+                OWNER VARCHAR NOT NULL);
+              """,
+        )
+    except Exception as e:
+        print(f"we have an error: {e}")
