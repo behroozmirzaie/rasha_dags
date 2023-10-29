139d5505fb65ae16584029c3c9085965c1e8007e
behrooz <mirzaie2@gmail.com>
2023-10-29 17:56:24 +0100

    Add task to get and show data

---
+++ training/postgret_get_and_show.py
@@ -0,0 +1,48 @@
+import datetime as dt
+
+import psycopg2
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'behrooz',
+    'start_date': dt.datetime.now(),
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=1),
+    'schedule_interval': dt.timedelta(minutes=1),
+    'conn_id': 'postgres_get_and_show'
+}
+
+dag = DAG(
+    'retrieve_and_pass_sql_query',
+    default_args=default_args,
+    schedule_interval='@daily',
+)
+
+
+def get_postgres_data():
+    conn = psycopg2.connect(host='postgresql', database='data_source', user='postgres', password='rasha_password')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM taxi_trips limit 100000')
+    rows = cur.fetchall()
+    return rows
+
+
+def show_rows(rows):
+    for row in rows:
+        print(row)
+
+
+get_postgres_data_operator = PythonOperator(
+    task_id='get_and_show_data',
+    python_callable=get_postgres_data,
+    dag=dag,
+)
+
+show_rows_operator = PythonOperator(
+    task_id='convert_to_duckdb',
+    python_callable=show_rows,
+    dag=dag,
+)
+
+get_postgres_data_operator >> show_rows_operator
