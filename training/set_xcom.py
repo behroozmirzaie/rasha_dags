f912c30ac6a892619829d8e7a4442ce7283b1e89
behrooz <mirzaie2@gmail.com>
2023-10-30 20:45:00 +0100

    Add get variable

--- training/set_xcom.py
+++ training/set_xcom.py
@@ -11,12 +11,21 @@ def set_variable(ti):
                  value="spark://spark-1698694077-master-0.spark-1698694077-headless.default.svc.ai.rasha.local:7077")
 
 
+def get_variable(ti):
+    print(f"this is value of SPARK_URL:{ti.xcom_pull(key='SPARK_URL')}")
+
+
 with DAG('xcom_dag',
          start_date=datetime(2023, 10, 30),
          schedule='@daily',
          catchup=False):
-    training_model_task = PythonOperator(
+    set_spark_url = PythonOperator(
         task_id=f'set_spark_url',
         python_callable=set_variable,
         do_xcom_push=True
     )
+    get_spark_url = PythonOperator(
+        task_id="get_spark_url",
+        python_callable=get_variable,
+    )
+    set_spark_url >> get_spark_url
